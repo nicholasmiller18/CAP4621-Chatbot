e7cb13dff18e7822d30eb934693ed3965bee0756
jonweese <36482468+jonweese@users.noreply.github.com>
2020-11-18 21:39:17 -0500

    Add files via upload

--- Model.py
+++ Model.py
@@ -3,16 +3,14 @@ import tensorflow as tf
 from tensorflow import keras
 
 if __name__ == "__main__":
-    #Create encoder network
-    encodermodel = keras.layers.Sequential([
-        keras.layers.Flatten(input_shape=(100,1)),
-        keras.layers.Dense(64,activation=tf.nm.relu),
-        keras.layers.Dense(100,activation=tf.nm.softmax)])
-
-    #Create decoder network
-    decodermodel = keras.layers.Sequential([
+    #Create neural network
+    model = keras.layers.Sequential([
         keras.layers.Flatten(input_shape=(100,1)),
         keras.layers.Dense(64,activation=tf.nm.relu),
         keras.layers.Dense(100,activation=tf.nm.softmax)])
 
     #save both of the models to the computer so they can be accessed by train.py and inference.py later
+    saver = tf.train.Saver()
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    saver.save(sess, 'my_test_model')
